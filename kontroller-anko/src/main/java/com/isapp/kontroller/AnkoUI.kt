ddb809ddd1af860d4e9f6199fbf1993f0660891d
Eliezer Graber <eygraber@gmail.com>
2017-02-04 23:46:53 -0500

    Make AnkoUI an interface and rename withContext to usingAnko

--- kontroller-anko/src/main/java/com/isapp/kontroller/AnkoUI.kt
+++ kontroller-anko/src/main/java/com/isapp/kontroller/AnkoUI.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.View
 import org.jetbrains.anko.AnkoContext
 
-abstract class AnkoUI<in T : UIController>(private val context: Context) : UI<T> {
-  fun withContext(ui: AnkoContext<Context>.() -> View) = AnkoContext.create(context, context).ui()
+interface AnkoUI<in T : UIController> : UI<T> {
+  val context: Context
+
+  fun usingAnko(ui: AnkoContext<Context>.() -> View) = AnkoContext.create(context, context).ui()
 }
