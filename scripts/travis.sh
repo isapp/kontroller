d834d196059b6317f1051f55512d93eb07418fde
Eliezer Graber <eygraber@gmail.com>
2017-02-05 16:23:18 -0500

    Bump version after releasing

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,9 +2,14 @@
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   if [ "${TRAVIS_BRANCH}" = "master" ]; then
-	./gradlew deployToBintraySnapshot
+    if [ "${TRAVIS_COMMIT_MESSAGE}" = "Version bump" ]; then
+	  echo "Skipping version bump commit"
+	else
+	  ./gradlew deployToBintraySnapshot
+	fi
   elif [ "${TRAVIS_BRANCH}" = "release" ]; then
     ./gradlew deployToBintray
+    ./gradlew bumpVersion
   fi
 else
   ./gradlew check
