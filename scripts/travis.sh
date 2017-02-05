8bf82faa480ee94f0ef2382c63011717442e49b3
Eliezer Graber <eygraber@gmail.com>
2017-02-05 03:54:21 -0500

    Deploy a release version when CI runs on the release branch

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,6 +3,8 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   if [ "${TRAVIS_BRANCH}" = "master" ]; then
 	./gradlew deployToBintraySnapshot
+  elif [ "${TRAVIS_BRANCH}" = "release" ]; then
+    ./gradlew deployToBintray
   fi
 else
   ./gradlew check
