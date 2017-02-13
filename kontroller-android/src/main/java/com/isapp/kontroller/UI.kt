52ac9c44e4dc4a648b3a7d25ab97738871d2fa3e
Eliezer Graber <eygraber@gmail.com>
2017-02-13 00:58:48 -0500

    Add documentation

--- kontroller-android/src/main/java/com/isapp/kontroller/UI.kt
+++ kontroller-android/src/main/java/com/isapp/kontroller/UI.kt
@@ -2,6 +2,18 @@ package com.isapp.kontroller
 
 import android.view.View
 
+/**
+ * An abstraction around a user interface.
+ *
+ * All UI related logic should reside in implementations.
+ *
+ * @param T the type of object that will be passed to [createView]; it is the `UI's` interface with the [UIController].
+ */
 interface UI<in T> {
+  /**
+   * Creates a [View] to be handled by the host of the [UIController].
+   *
+   * @param controller an interface to the `Controller`
+   */
   fun createView(controller: T): View?
 }
--- kontroller-anko/src/main/java/com/isapp/kontroller/AnkoUI.kt
+++ kontroller-anko/src/main/java/com/isapp/kontroller/AnkoUI.kt
@@ -3,9 +3,27 @@ package com.isapp.kontroller
 import android.content.Context
 import android.view.View
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.UI
 
+/**
+ * A [UI] with a convenience function for creating an [AnkoContext].
+ */
 interface AnkoUI<in T> : UI<T> {
+  /**
+   * The [Context] that will be used to create the [AnkoContext].
+   */
   val context: Context
 
+  /**
+   * A convenience function for creating an [AnkoContext].
+   *
+   * ```
+   * class SampleAnkoUI(override val context = context) : AnkoUI<Sample> {
+   *   override fun createView(controller: Sample) = usingAnko {
+   *     frameLayout()
+   *   }
+   * }
+   * ```
+   */
   fun usingAnko(ui: AnkoContext<Context>.() -> View) = AnkoContext.create(context, context).ui()
 }
